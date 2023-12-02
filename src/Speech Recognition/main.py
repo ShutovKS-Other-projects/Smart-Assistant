a00544595513655b1c4c6c2abd16d9677c2f9055
ShutovKS <schutow.kir@yandex.ru>
2023-12-02 23:14:52 +0300

    Added speech recognition, tool of python

---
+++ src/Speech Recognition/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from huggingsound import SpeechRecognitionModel
+
+app = Flask(__name__)
+
+model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian")
+
+
+@app.route('/transcribe_audio', methods=['POST'])
+def transcribe_audio():
+    try:
+        # Получаем файл mp3 из запроса
+        audio_file = request.files['audio']
+
+        # Сохраняем файл временно (может потребоваться изменить путь)
+        audio_path = "temp_audio.mp3"
+        audio_file.save(audio_path)
+
+        # Передаем файл в модель распознавания голоса
+        transcriptions = model.transcribe([audio_path])
+
+        # Возвращаем результат в ответ
+        return jsonify({'status': 'success', 'transcriptions': transcriptions})
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
