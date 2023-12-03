a6e4a07d10eef24421a7d71d2c01585386bbf305
ShutovKS <schutow.kir@yandex.ru>
2023-12-03 18:59:25 +0300

    Added conversational request in Unity

---
+++ src/Smart Assistant/Assets/Scripts/Units/Conversational/ConversationalRequest.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Units.Conversational
+{
+    public class ConversationalRequest
+    {
+        private const string SERVER_URL = "http://127.0.0.1:5002/generate_response";
+
+        public static async Task<string> GenerateResponseAsync(string userInput)
+        {
+            var form = new WWWForm();
+            form.AddField("user_input", "@@ПЕРВЫЙ@@" + userInput + "@@ВТОРОЙ@@");
+
+            var www = UnityWebRequest.Post(SERVER_URL, form);
+            await www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                var jsonResponse = www.downloadHandler.text;
+                var response = JsonUtility.FromJson<ResponseData>(jsonResponse);
+
+                if (response.status == "success")
+                {
+                    var generatedResponse = response.generated_response;
+                    return generatedResponse;
+                }
+
+                Debug.LogError("Error: " + response.message);
+                return null;
+            }
+
+            Debug.LogError("Request failed. Error: " + www.error);
+            return null;
+        }
+
+        [System.Serializable]
+        public class ResponseData
+        {
+            public string status;
+            public string generated_response;
+            public string message;
+        }
+    }
+}
